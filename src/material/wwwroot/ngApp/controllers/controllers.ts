b3b66d21cc410fdd42f7685f8a235ee74a51b3f8
Stephen C. Boatman <scboatman@outlook.com>
2016-10-28 10:12:06 -0700

    Material

---
+++ src/material/wwwroot/ngApp/controllers/controllers.ts
@@ -0,0 +1,13 @@
+namespace material.Controllers {
+
+    export class HomeController {
+        public message = 'Hello from the home page!';
+    }
+
+
+
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+}
---
+++ src/material/wwwroot/ngApp/services/services.ts
@@ -0,0 +1,3 @@
+namespace material.Services {
+
+    }
